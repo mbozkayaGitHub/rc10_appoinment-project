140e40d5b30c8d052c7840c0f8e538ea662cddc1
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-05 15:28:08 +0300

    Appointment-Project

---
+++ src/components/Doctors.jsx
@@ -0,0 +1,15 @@
+import Container from "react-bootstrap/Container"
+
+
+
+  
+  const Doctors = ({doctors}) => {
+    return (
+     <Container className="p-2">
+      <h1>Doctors</h1>
+      </Container>
+    )
+  }
+  
+  
+export default Doctors
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from "react"
+import AppointmentList from "../components/AppointmentList"
+import Doctors from "../components/Doctors"
+import { doctorData } from "../helper/data"
+
+
+const Home = () => {
+
+const [doctors, setDoctors] = useState(doctorData)
+
+  return (
+    <main className="text-center mt-2">
+      <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
+    <Doctors doctors={doctors}/>
+    <AppointmentList/>
+    </main>
+  )
+}
+
+export default Home
